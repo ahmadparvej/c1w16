d1708b4af5d6216102f8c750a86aed972b79ff1d
Parvej Ahmad <81608589+ahmadparvej@users.noreply.github.com>
2022-04-11 14:21:23 +0530

    c1 test

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -1,5 +1,14 @@
 // reusable card component
-const GroceryItem = () => {
-  return <></>;
+const GroceryItem = (props) => {
+  console.log(props)
+  return <>
+      {/* <div>
+        <p>{props.discount}</p>
+        <img src={props.imgURL} alt="" />
+        <p>{props.mrp}</p>
+        <p>{props.sellingPrice}</p>
+        <p>{props.title}</p>
+      </div> */}
+  </>;
 };
 export default GroceryItem;
